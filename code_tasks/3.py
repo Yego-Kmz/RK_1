c9a157018c65cf7d11376ccd93be4236f0e1470b
Yego-Kmz <70777296+Yego-Kmz@users.noreply.github.com>
2021-10-03 23:15:55 +0300

    задача 3

--- code_tasks/3.py
+++ code_tasks/3.py
@@ -1,9 +1,9 @@
 import json
 import random
 import requests
-
 from http.server import BaseHTTPRequestHandler
 from helpers import http_server
+from domonic import *
 
 # urllib3 for url parse
 
@@ -13,10 +13,41 @@ class BookHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print('Get request received')
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        resp = requests.get(f"http://numbersapi.com/{random.randint(0, 100)}")
-        self.wfile.write(json.dumps({'status': resp.status_code, 'content': resp.content.decode()}).encode())
+        number = random.randint(0, 100)
+        resp = requests.get(f"http://numbersapi.com/{number}")
+        respon = render(
+            html(
+                head(
+                ),
+                body(
+                    style(
+                        'BODY {background: url(https://capital2020.ru/d/mgtu_im_n_e_baumana_moskva.jpg) no-repeat; margin: 0px};',
+                        _type="text/css",
+                        ),
+                    header(b('Random interesting fact about ', number,
+                    _style =
+                    '''font-size: 30px;
+                    color: green;
+                    position: absolute;
+                    width: 100%;
+                    text-align: center;
+                    top: 5%;
+                    ''')),
+                    p(resp.content.decode(),
+                      _style=
+                      '''font-size: 20px;
+                      position: absolute;
+                      text-align: center;
+                      width: 100%;
+                      top: 15%;
+                      ''')
+                )
+            )
+        )
+        self.wfile.write(respon.encode())
+
 
 
 if __name__ == "__main__":
